5c2d38c9582957f2aa736d21458b8bf38861ce25
Pablo Laso <plasocortabitarte@gmail.com>
2022-04-06 20:30:59 +0200

    feat: login

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,10 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ ok: true })
 })
 
+/* Auth */
+
+router.post('/login', authController.login)
+
 /* Users */
 
 router.post('/users', authController.create)
